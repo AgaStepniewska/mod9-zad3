0f6b8899420d5dc9c27f46340a05a79dbfb043fb
pinko <agaslota@wp.pl>
2017-06-05 13:13:29 +0100

    połowa zdania z użyciem length/2

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ var dinosaurUpperCased = dinosaur.toUpperCase();
 console.log(dinosaurUpperCased);
 
 var textCharsAfter = text.replace('Velociraptor', dinosaurUpperCased);
-console.log(textCharsAfter.length/2);
-var textCharsAfter2 = textCharsAfter.substr(0,72);
+/*rany, to jest dla mnie totalna magia. 
+Nie wiem jakim cudem udało mi się to wymyśleć ;) */
+var textCharsAfter2 = textCharsAfter.substr(0,textCharsAfter.length/2);
 console.log(textCharsAfter2);
